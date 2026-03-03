7861badb0e986614823cf371e2bd2dc60fd2ebc7
Alex Skorulis <skorulis@gmail.com>
2026-03-03 19:00:09 +1100

    Add more snapshot tests

---
+++ ItemsSnapshotTests/Scene/WarehouseViewSnapshotTests.swift
@@ -0,0 +1,58 @@
+// Created by Cursor on 3/3/2026.
+
+@testable import Items
+import Knit
+import SnapshotTesting
+import SwiftUI
+import Testing
+
+@MainActor
+struct WarehouseViewSnapshotTests {
+
+    @Test
+    func warehouse_empty_items() {
+        let assembler = ItemsAssembly.testing()
+        let viewModel = assembler.resolver.warehouseViewModel()
+        let view = WarehouseView(viewModel: viewModel)
+
+        assertSnapshot(of: view, as: .image(on: .iPhoneSe))
+    }
+
+    @Test
+    func warehouse_with_items() {
+        let assembler = ItemsAssembly.testing()
+        let mainStore = assembler.resolver.mainStore()
+
+        mainStore.warehouse.add(item: .apple, count: 5)
+        mainStore.warehouse.add(item: .silverFlorin, count: 3)
+        mainStore.warehouse.add(item: .goldFlorin, count: 1)
+
+        let viewModel = assembler.resolver.warehouseViewModel()
+        let view = WarehouseView(viewModel: viewModel)
+
+        assertSnapshot(of: view, as: .image(on: .iPhoneSe))
+    }
+
+    @Test
+    func warehouse_artifacts_tab_with_equipped() {
+        let assembler = ItemsAssembly.testing()
+        let mainStore = assembler.resolver.mainStore()
+
+        // Unlock artifacts tab.
+        mainStore.achievements.add(achievements: [.artifact1])
+
+        // Add and equip a couple of artifacts.
+        mainStore.warehouse.add(artifact: ArtifactInstance(type: .frictionlessGear, quality: .good))
+        mainStore.warehouse.add(artifact: ArtifactInstance(type: .luckyCoin, quality: .common))
+        mainStore.warehouse.equip(.frictionlessGear)
+        mainStore.warehouse.equip(.luckyCoin)
+
+        let viewModel = assembler.resolver.warehouseViewModel()
+        viewModel.page = .artifacts
+
+        let view = WarehouseView(viewModel: viewModel)
+
+        assertSnapshot(of: view, as: .image(on: .iPhoneSe))
+    }
+}
+
