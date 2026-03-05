48763bc844110cf8d0a238189ed4bb063fba767f
Alex Skorulis <skorulis@gmail.com>
2026-03-05 20:50:59 +1100

    Add animations to CardPathWrapper

--- Items/Scene/Coordinator/CardPathWrapper.swift
+++ Items/Scene/Coordinator/CardPathWrapper.swift
@@ -8,21 +8,36 @@ struct CardPathWrapper<Content: View>: View {
     let content: () -> Content
     
     @Environment(\.dismissCustomOverlay) private var onDismiss
+    @State private var isVisible: Bool = false
+    @State private var animatingOut: Bool = false
     
     var body: some View {
         ZStack {
             Color.black.opacity(0.3)
                 .onTapGesture(perform: maybeDismiss)
                 .ignoresSafeArea()
+                .opacity(animatingOut ? 0 : 1)
             
             content()
                 .background(CardBackground())
                 .padding(16)
+                .opacity(isVisible ? 1 : 0)
+                .scaleEffect(isVisible ? 1 : 0.9)
+                .onAppear {
+                    withAnimation(.snappy(duration: 0.25)) {
+                        isVisible = true
+                    }
+                }
         }
     }
     
     private func maybeDismiss() {
-        onDismiss()
+        withAnimation(.snappy(duration: 0.25)) {
+            isVisible = false
+            animatingOut = true
+        } completion: {
+            onDismiss()
+        }
     }
     
 }
