a5b67872d4a04c0daf7bf488e697bb2186b3e307
Alex Skorulis <skorulis@gmail.com>
2026-03-09 13:14:20 +1100

    Create a models package

---
+++ Modules/Models/Sources/Models/ClientRequest.swift
@@ -0,0 +1,9 @@
+//  Created by Alexander Skorulis on 9/3/2026.
+
+import Foundation
+
+// Requests send to the client
+public enum ClientRequest: Codable {
+    
+    case getWarehouse
+}
---
+++ Modules/Models/Sources/Models/ClientResponse.swift
@@ -0,0 +1,7 @@
+//  Created by Alexander Skorulis on 9/3/2026.
+
+import Foundation
+
+public enum ClientResponse {
+    
+}
